0dc9af8f9ff7d6a2b48f63c46a593cd893505bdb
tomek <masterworks41@gmail.com>
2015-11-25 19:17:24 +0100

    base functionality url connecting

---
+++ app/src/main/java/com/tomek/luckynumber/LuckyNumber.java
@@ -0,0 +1,44 @@
+package com.tomek.luckynumber;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.io.IOException;
+import org.jsoup.nodes.Element;
+
+public class LuckyNumber
+{
+	private static final String URL_STRING = "http://lo-lubaczow.pl/";
+
+	public static String parseHtml(Document main_page)
+	{
+		Element image = main_page.select("div.grid_1.numerki").first();
+		Element img = image.children().first();
+		String img_url = img.attr("src");
+		return img_url.substring(img_url.lastIndexOf("m")+1, img_url.indexOf("."));
+	}
+	
+	public static int getLucky() throws IOException
+	{
+		Document main_page = Jsoup.connect(URL_STRING).get();
+		String img_number = parseHtml(main_page);
+		return Integer.parseInt(img_number);
+	}
+	public static void printLucky()
+	{
+		int lucky = 0;
+		try
+		{
+			lucky = getLucky();
+		}
+		catch(IOException ex)
+		{
+			System.out.println(ex.toString());
+		}
+		System.out.println("Your lucky number is " + lucky);
+	}
+
+	public static void main(String[] args)
+	{
+		printLucky();
+	}
+}
--- app/src/main/java/com/tomek/luckynumber/MainActivity.java
+++ app/src/main/java/com/tomek/luckynumber/MainActivity.java
@@ -1,31 +1,69 @@
 package com.tomek.luckynumber;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView luckyText;
+    private int luckyNumber;
+    private FloatingActionButton fab;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        luckyText = ((TextView) findViewById(R.id.lucky_text));
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+                new CheckNumber().execute();
+
+                Log.d("onClickpoExeceute", "number : " + luckyNumber + " text : " + luckyText);
+
+                }
         });
+    }
+
+    class CheckNumber extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                luckyNumber = LuckyNumber.getLucky();
+                Log.d("doInBackground", "number : " + luckyNumber + " text : " + luckyText);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            luckyText.setText(String.valueOf(luckyNumber));
+            Log.d("onPostExecute", "number : " + luckyNumber + " text : " + luckyText);
+            String message = "";
+            if (luckyNumber == 0) {
+                message = "Błąd !";
+            }
+            else message = "Pomyslnie pobrano numerek !";
+            Snackbar.make(fab, message, Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }
     }
 
     @Override
